cfce4eeb89a5ba1e94262eb5ed4204eb39916420
James Hawk <jph94880@gmail.com>
2024-01-01 13:37:27 -0600

    Adds search event listener

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 /* Table will display all dhoes available for purchase. After the search, the table will display the 'matching shoes'*/
 // Make a function for each functionality that you want. 
 
-const testing = console.log("test")
+
 
 // 1:25
 
@@ -10,6 +10,18 @@ document.addEventListener("DOMContentLoaded", () => {
   console.log("DOMContentLoaded complete!")
 })
 
+
+document.querySelector("#searchBar").addEventListener("search", (event) => {
+  console.log("User typed...", event.target.value);
+  const searchTerm = event.target.value;
+  searchShoes(searchTerm)
+})
+
+function searchShoes(searchTerm) {
+  fetch("http://localhost:3000/shoes")
+  .then((r) => r.json())
+  .then(console.log)
+}
 
 function getAllShoes() {
   return fetch('http://localhost:3000/shoes')
@@ -36,11 +48,9 @@ displayAllShoes()
 
 
 function displayOneShoe(shoe) {
-  console.log("I should display", shoe)
   const shoeCell = document.createElement('li')
   const image = document.createElement('img')
   image.src = shoe.image
-  console.log(image)
   shoeCell.append(image)
   const shoesGridUl = document.getElementById('shoeGrid')
   shoesGridUl.append(shoeCell)
