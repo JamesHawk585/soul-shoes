a8fe7e7c35c346fa707ff757b8cef278f75c4b5d
James Hawk <jph94880@gmail.com>
2024-01-03 21:36:07 -0600

    appends [object HTMLButtonElement],[object HTMLButtonElement] to shoeCell

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # soul-shoes
+We Sell Shoes 👟
+
+1. [✔] Display shoes that match a given search term.
+2. [✔] Add links to footer. 
+2. [] Add name and price to each li appended to shoesGridUl. 
--- index.js
+++ index.js
@@ -7,9 +7,8 @@
 // 1:25
 
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("DOMContentLoaded complete!")
+  return
 })
-
 
 document.querySelector("#searchBar").addEventListener("search", (event) => {
   const searchTerm = event.target.value;
@@ -24,7 +23,7 @@ function searchShoes(searchTerm) {
     // Compare it with search term
     // display matches 
   const filteredShoes = shoes.filter(shoe => {
-    if(shoe.brand === searchTerm || shoe.style === searchTerm || shoe.color[0] === searchTerm || shoe.color[1] === searchTerm) {
+    if(shoe.brand === searchTerm || shoe.style === searchTerm || shoe.colors[0] === searchTerm || shoe.colors[1] === searchTerm) {
     return true;
     } else {
     return false;
@@ -44,15 +43,42 @@ function getAllShoes() {
 
 
 function displayOneShoe(shoe) {
-  console.log(shoe)
   const shoeCell = document.createElement('li')
   const image = document.createElement('img')
+  const shoeBrandH3 = document.createElement('h3')
+  const paragraph = document.createElement('p')
   image.src = shoe.image
+  // We are getting a complete shoe object here. Currently, the image property of the shoe object is being set as an attribute of the newly created shoe li element.
+  // Append two additional li elements to shoeCell, name and price. 
+  // Create at least one additional function?  
   shoeCell.append(image)
+  shoeCell.append(shoeBrandH3)
+  shoeCell.append(paragraph)
+  shoeBrandH3.innerText = `${shoe.brand}`
+  paragraph.innerText = `Price: $${shoe.price}`
+  const colorSelector = createColorSelector(shoe.colors) 
+  shoeCell.append(colorSelector)
   const shoesGridUl = document.getElementById('shoeGrid')
   shoesGridUl.append(shoeCell)
   // shoesGridUl.append(image)
 }
+
+function createColorSelector(colors) {
+  const colorButtons = colors.map(color => createColorButton(color)) 
+  console.log("colorButtons", colorButtons)
+  const div = document.createElement("div")
+  div.append(colorButtons);
+
+  return div;
+}
+
+// 1:36:09
+
+function createColorButton(color) {
+  const colorButton = document.createElement("button") 
+  return colorButton
+}
+
 
 function resetShoeGrid() {
   document.getElementById('shoeGrid').replaceChildren();
